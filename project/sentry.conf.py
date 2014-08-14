21acbbfde09175146f299387627d615a4db1a49e
Valery Sukhomlinov <good-guy@good-guy.me>
2014-08-14 21:31:16 +0400

    removed unnecessary path

--- project/sentry.conf.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Sentry server configuration
-# Real settings are only imported here - change them in server.conf.py
-
-# Importing sentry settings
-from server.conf.py import *
-
-# Importing server libraries to run as a WSGI script
-from sentry.conf.server import *
